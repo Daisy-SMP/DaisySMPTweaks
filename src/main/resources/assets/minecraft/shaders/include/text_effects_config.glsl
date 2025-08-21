406430873fc39d1f34ae90331d78444f2d580b5a
macuguita <raulpripri@gmail.com>
2025-08-21 20:23:15 +0200

    now has roles copied from daisy-1

---
+++ src/main/resources/assets/minecraft/shaders/include/text_effects_config.glsl
@@ -0,0 +1,264 @@
+// Owner tag
+TEXT_EFFECT(240, 105, 0) {
+    apply_shimmer(0.25, 0.5);
+    apply_gradient(rgb(254, 225, 152), rgb(191, 143, 23));
+}
+
+// Co-Owner tag
+TEXT_EFFECT(240, 105, 4) {
+    apply_shimmer(0.25, 0.5);
+    apply_gradient(rgb(163, 185, 247), rgb(89, 120, 207));
+}
+
+// Admin tag
+TEXT_EFFECT(240, 105, 8) {
+    apply_shimmer(0.25, 0.5);
+    apply_gradient(rgb(250, 175, 202), rgb(207, 87, 130));
+}
+
+// Moderator tag
+TEXT_EFFECT(240, 105, 12) {
+    apply_shimmer(0.25, 0.5);
+    apply_gradient(rgb(162, 148, 239), rgb(85, 67, 181));
+}
+
+// Daisy SMP effect
+TEXT_EFFECT(240, 105, 16) {
+    override_text_color(rgb(237, 205, 25));
+    remove_text_shadow();
+    apply_outline(rgb(255, 255, 255));
+}
+
+// Poppy
+TEXT_EFFECT(240, 105, 20) {
+    override_text_color(rgb(223, 61, 61));
+    override_shadow_color(rgb(100, 20, 56));
+}
+
+// Lily 1
+TEXT_EFFECT(240, 105, 24) {
+    override_text_color(rgb(41, 94, 200));
+    apply_outline(rgb(18, 67, 163));
+}
+
+// Lily 2
+TEXT_EFFECT(240, 105, 28) {
+    override_text_color(rgb(243, 159, 243));
+    apply_outline(rgb(198, 112, 198));
+}
+
+// Lily 3
+TEXT_EFFECT(240, 105, 32) {
+    override_text_color(rgb(249, 138, 40));
+    apply_outline(rgb(183, 90, 8));
+}
+
+// Orchid 1
+TEXT_EFFECT(240, 105, 36) {
+    override_text_color(rgb(82, 255, 252));
+    apply_thin_outline(rgb(53, 154, 221));
+}
+
+// Orchid 2
+TEXT_EFFECT(240, 105, 40) {
+    override_text_color(rgb(217, 150, 233));
+    apply_thin_outline(rgb(152, 71, 153));
+}
+
+// Orchid 3
+TEXT_EFFECT(240, 105, 44) {
+    override_text_color(rgb(70, 239, 68));
+    apply_thin_outline(rgb(24, 148, 22));
+}
+
+// Daisy 1
+TEXT_EFFECT(240, 105, 48) {
+    apply_gradient(rgb(255, 179, 179), rgb(221, 72, 72));
+    remove_text_shadow();
+    apply_outline(rgb(176, 48, 48));
+}
+
+// Daisy 2
+TEXT_EFFECT(240, 105, 52) {
+    apply_gradient(rgb(255, 212, 164), rgb(226, 138, 38));
+    remove_text_shadow();
+    apply_outline(rgb(206, 117, 16));
+}
+
+// Daisy 3
+TEXT_EFFECT(240, 105, 56) {
+    apply_gradient(rgb(255, 247, 205), rgb(246, 218, 57));
+    remove_text_shadow();
+    apply_outline(rgb(231, 181, 17));
+}
+
+// Daisy 4
+TEXT_EFFECT(240, 105, 60) {
+    apply_gradient(rgb(189, 255, 158), rgb(105, 237, 44));
+    remove_text_shadow();
+    apply_outline(rgb(66, 176, 15));
+}
+
+// Daisy 5
+TEXT_EFFECT(240, 105, 64) {
+    apply_gradient(rgb(167, 234, 255), rgb(30, 174, 240));
+    remove_text_shadow();
+    apply_outline(rgb(13, 136, 194));
+}
+
+// Daisy 6
+TEXT_EFFECT(240, 105, 68) {
+    apply_gradient(rgb(205, 171, 255), rgb(189, 72, 231));
+    remove_text_shadow();
+    apply_outline(rgb(127, 32, 194));
+}
+
+// Daisy 7
+TEXT_EFFECT(240, 105, 72) {
+    apply_gradient(rgb(255, 218, 233), rgb(255, 129, 175));
+    remove_text_shadow();
+    apply_outline(rgb(214, 86, 133));
+}
+
+// Daisy 8
+TEXT_EFFECT(240, 105, 76) {
+    apply_gradient(rgb(255, 255, 255), rgb(0, 0, 0));
+    remove_text_shadow();
+    apply_outline(rgb(0, 0, 0));
+}
+
+TEXT_EFFECT(240, 240, 0) {
+    apply_shaking_movement();
+    override_text_color(rgb(255, 82, 82));
+    override_shadow_color(rgb(100, 20, 80));
+}
+
+TEXT_EFFECT(240, 240, 4) {
+    apply_waving_movement();
+    override_text_color(rgb(255, 235, 60));
+    override_shadow_color(rgb(150, 60, 30));
+}
+
+TEXT_EFFECT(240, 240, 8) {
+    apply_iterating_movement();
+    override_text_color(rgb(86, 235, 86));
+    override_shadow_color(rgb(20, 80, 90));
+}
+
+TEXT_EFFECT(240, 240, 12) {
+    apply_flipping_movement();
+    override_text_color(rgb(74, 222, 209));
+    override_shadow_color(rgb(37, 71, 150));
+}
+
+TEXT_EFFECT(240, 240, 16) {
+    apply_skewing_movement();
+    override_text_color(rgb(122, 80, 251));
+    override_shadow_color(rgb(40, 40, 140));
+}
+
+TEXT_EFFECT(240, 240, 20) {
+    override_text_color(rgb(255, 82, 82));
+    apply_outline(rgb(100, 20, 80));
+}
+
+TEXT_EFFECT(240, 240, 24) {
+    apply_gradient(rgb(255, 235, 120), rgb(255, 82, 82));
+}
+
+TEXT_EFFECT(240, 240, 28) {
+    apply_rainbow();
+}
+
+TEXT_EFFECT(240, 240, 32) {
+    override_text_color(rgb(86, 235, 86));
+    override_shadow_color(rgb(20, 80, 90));
+    apply_shimmer();
+}
+
+TEXT_EFFECT(240, 240, 36) {
+    override_text_color(rgb(255, 255, 255));
+    apply_chromatic_abberation();
+    remove_text_shadow();
+}
+
+TEXT_EFFECT(240, 240, 40) {
+    apply_metalic(rgb(160, 160, 200));
+}
+
+TEXT_EFFECT(240, 240, 44) {
+    override_text_color(rgb(255, 20, 20));
+    apply_fire();
+}
+
+TEXT_EFFECT(240, 240, 48) {
+    apply_growing_movement();
+    override_text_color(rgb(255, 82, 82));
+    override_shadow_color(rgb(100, 20, 80));
+}
+
+TEXT_EFFECT(240, 240, 52) {
+    override_text_color(rgb(255, 235, 60));
+    override_shadow_color(rgb(150, 60, 30));
+    apply_fade(rgb(86, 235, 86));
+}
+
+TEXT_EFFECT(240, 240, 56) {
+    override_text_color(rgb(86, 235, 86));
+    override_shadow_color(rgb(20, 80, 90));
+    apply_blinking();
+}
+
+TEXT_EFFECT(240, 240, 60) {
+    override_text_color(rgb(74, 222, 209));
+    override_shadow_color(rgb(37, 71, 150));
+    apply_glowing();
+}
+
+TEXT_EFFECT(240, 240, 64) {
+    apply_lesbian_pride();
+}
+
+TEXT_EFFECT(240, 240, 68) {
+    apply_mlm_pride();
+}
+
+TEXT_EFFECT(240, 240, 72) {
+    apply_bisexual_pride();
+}
+
+TEXT_EFFECT(240, 240, 76) {
+    apply_transgender_pride();
+}
+
+TEXT_EFFECT(240, 240, 80) {
+    apply_pride();
+}
+
+TEXT_EFFECT(240, 240, 84) {
+    apply_pansexual_pride();
+}
+
+TEXT_EFFECT(240, 240, 88) {
+    apply_asexual_pride();
+}
+
+TEXT_EFFECT(240, 240, 92) {
+    apply_aromantic_pride();
+}
+
+TEXT_EFFECT(240, 240, 96) {
+    apply_non_binary_pride();
+}
+
+TEXT_EFFECT(94, 171, 136) {
+    apply_waving_movement(1.0, 1.5);
+    apply_gradient(rgb(189, 221, 100), rgb(50, 117, 132));
+    override_shadow_color(rgb(70, 70, 100));
+}
+
+TEXT_EFFECT(255, 255, 248) {
+    apply_vertical_shadow();
+    apply_metalic(rgb(255, 255, 255), rgb(150, 163, 177) * 0.95);
+    override_shadow_color(rgb(70, 70, 100));
+}
